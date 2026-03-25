7fa46f4c76e455f915db14b0c4bc40f9f25dee1a
Wojciech <tazor1337@gmail.com>
2026-03-25 19:40:37 +0100

    chore(Grecaptcha): global

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -3,9 +3,16 @@ import { AxiosInstance } from 'axios';
 import { route as ziggyRoute } from 'ziggy-js';
 import { PageProps as AppPageProps } from './';
 
+/** Google reCAPTCHA v3 (loaded from https://www.google.com/recaptcha/api.js) */
+interface Grecaptcha {
+  ready: (callback: () => void) => void;
+  execute: (siteKey: string, options: { action: string }) => Promise<string>;
+}
+
 declare global {
   interface Window {
     axios: AxiosInstance;
+    grecaptcha?: Grecaptcha;
   }
 
   /* eslint-disable no-var */
